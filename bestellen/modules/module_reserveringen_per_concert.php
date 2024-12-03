cc84b83745b412f73f7f4f8479a5a5df2690e7b6
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-12-03 14:21:54 +0100

    1

--- bestellen/modules/module_reserveringen_per_concert.php
+++ bestellen/modules/module_reserveringen_per_concert.php
@@ -8,6 +8,7 @@ session_start();
 require_once('modules/bestelfuncties.php');
 require_once($_SERVER["DOCUMENT_ROOT"] . '/vendor/autoload.php');
 use function PHP81_BC\strftime;
+use function kint\kint;
 
 Kint::$enabled_mode = true;
 
