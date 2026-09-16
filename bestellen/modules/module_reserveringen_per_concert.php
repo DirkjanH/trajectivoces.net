d1bbe3d8ed41cff36dc7acbab45ed7ee0d45e5af
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-09-16 20:30:28 +0200

    Fix require_once path for bestelfuncties.php in concert reservation module

--- bestellen/modules/module_reserveringen_per_concert.php
+++ bestellen/modules/module_reserveringen_per_concert.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 
 session_start();
 
-require_once('modules/bestelfuncties.php');
+require_once __DIR__ . '/bestelfuncties.php';
 require_once($_SERVER["DOCUMENT_ROOT"] . '/vendor/autoload.php');
 use function PHP81_BC\strftime;
 
